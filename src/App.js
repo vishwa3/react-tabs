c912003cc3955387020bd9032570827cf99bbf63
vishwa3 <vishwanathkrishnappa@gmail.com>
2024-09-25 19:37:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const data = [
+  {
+    value: "html",
+    label: "HTML",
+    description:
+      "The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser.",
+  },
+  {
+    value: "css",
+    label: "CSS",
+    description:
+      "Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML or XML.",
+  },
+  {
+    value: "javascript",
+    label: "JavaScript",
+    description:
+      "JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS.",
+  },
+];
 
 function App() {
+  const [tabState, setTabState] = useState("html");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app_container">
+      <div className="tabs_container">
+        {data.map(({ value, label }) => (
+          <button
+            key={value}
+            className={tabState === value && "active"}
+            onClick={() => setTabState(value)}
+          >
+            {label}
+          </button>
+        ))}
+      </div>
+      <div className="tab_description">
+        {data.map(({ description, value }) => (
+          <p key={value} hidden={value !== tabState}>
+            {description}
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
